f4d162e77602948f94beae04085165e07125b536
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2025-09-03 12:01:23 +0530

    added code writen in java lab

---
+++ java_lab/java_3/Lab1P1.java
@@ -0,0 +1,71 @@
+class Lab1P1{
+       public static void main(String...args){
+                byte b = 15;
+                Byte bb = b;
+                
+           //  int x = 200;
+           //  byte b = x;
+                System.out.println(Byte.MAX_VALUE);
+                System.out.println(bb.MIN_VALUE);
+                System.out.println(bb.SIZE);
+                System.out.println(bb.TYPE);
+                System.out.println(bb.BYTES);
+                System.out.println("----Short---");
+
+                short s=10;
+                Short ss=s;
+
+                System.out.println(ss.MAX_VALUE);
+                System.out.println(ss.MIN_VALUE);
+                System.out.println(ss.SIZE);
+                System.out.println(ss.TYPE);
+                System.out.println(ss.BYTES);
+                System.out.println("----Integer---");
+
+                int i = 15;
+                Integer ii = i;
+
+                System.out.println(Integer.MAX_VALUE);
+                System.out.println(ii.MIN_VALUE);
+                System.out.println(ii.SIZE);
+                System.out.println(ii.TYPE);
+                System.out.println(ii.BYTES);
+            
+
+       }
+}class Lab1P1{
+       public static void main(String...args){
+                byte b = 15;
+                Byte bb = b;
+                
+           //  int x = 200;
+           //  byte b = x;
+                System.out.println(Byte.MAX_VALUE);
+                System.out.println(bb.MIN_VALUE);
+                System.out.println(bb.SIZE);
+                System.out.println(bb.TYPE);
+                System.out.println(bb.BYTES);
+                System.out.println("----Short---");
+
+                short s=10;
+                Short ss=s;
+
+                System.out.println(ss.MAX_VALUE);
+                System.out.println(ss.MIN_VALUE);
+                System.out.println(ss.SIZE);
+                System.out.println(ss.TYPE);
+                System.out.println(ss.BYTES);
+                System.out.println("----Integer---");
+
+                int i = 15;
+                Integer ii = i;
+
+                System.out.println(Integer.MAX_VALUE);
+                System.out.println(ii.MIN_VALUE);
+                System.out.println(ii.SIZE);
+                System.out.println(ii.TYPE);
+                System.out.println(ii.BYTES);
+            
+
+       }
+}
