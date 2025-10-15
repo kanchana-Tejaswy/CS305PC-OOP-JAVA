303a5dccc234ea57b844a63c8cb43e170a96f9d2
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2025-10-15 10:45:23 +0530

    renamed class name as lab5p1

--- Lab5P1.java
+++ Lab5P1.java
@@ -5,7 +5,7 @@ import java.math.*;
 import java.util.regex.*;
 import java.util.Scanner;
 
-public class Solution {
+public class Lab5P1 {
 
     public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
