4dc2e2fb9adb52a0a5526158c3ae80fffa6dc644
Kanchana.Tejawy <mail2tejaswy@gmail.com>
2025-10-29 19:51:46 +0530

    Fixed error
    Refactored main method to handle various operators and added input handling.

--- Lab2P1.java
+++ Lab2P1.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 class Lab2P1{
-		public static void main(String...args){
-					Scanner s=new Scanner(System.in);
-					int x = s.nextline();
-					int y=x;
-					System.out.println("output is:"+ x++ + ++x);
-					x=y;
-					System.out.println("output is:"+(x++ + ++x));
-					x=y;
-					System.out.println(+ x++ + ++x"output is:");
-		}
+	public static void main(String...args){
+	
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	s.nextLine();
+	String op = s.nextLine();
+	switch(op){
+	case "++":
+		    System.out.println("Post : "+(a++));
+		    System.out.println("Pre : "+(++a));
+		    break;
+	case "--":
+		    System.out.println("Post : "+(a--));
+		    System.out.println("Pre : "+(--a));
+		    break;
+	case "+":
+	            a = +a;
+	            System.out.println("Unary +:"+(a));
+		    break;
+	case "-":
+	            a = -a;
+	            System.out.println("Unary +:"+(-a));
+		    break;
+	case "~":   
+	            System.out.println("Unary +:"+(~a));
+		    break;
+	default :   	 
+                    System.out.println("sorry you entered wrong operator");
+}
+}
 }
